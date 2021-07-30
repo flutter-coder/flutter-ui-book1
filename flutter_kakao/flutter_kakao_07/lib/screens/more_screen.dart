c65c322088bc0a0e0333f7ce03feedd89a0e5ea1
flutter-coder <74853181+flutter-coder@users.noreply.github.com>
2021-07-30 09:36:31 +0900

    Column 제거

--- flutter_kakao/flutter_kakao_07/lib/screens/more_screen.dart
+++ flutter_kakao/flutter_kakao_07/lib/screens/more_screen.dart
@@ -1,31 +1,29 @@
-import 'package:flutter_kakao/models/tap.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/models/tab.dart';
 
 class MoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(title: Text("더보기")),
-      body: Column(
-        children: [
-          SizedBox(height: 30),
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 4, // 1
-              children: List.generate(
-                taps.length,
-                (index) => Column(
-                  children: [
-                    Icon(taps[index].icon),
-                    SizedBox(height: 5),
-                    Text(taps[index].text),
-                  ],
-                ),
-              ),
+      appBar: AppBar(
+        title: Text("더보기"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: GridView.count(
+          crossAxisCount: 4, // ➊
+          children: List.generate(
+            tabs.length,
+            (index) => Column(
+              children: [
+                Icon(tabs[index].icon),
+                SizedBox(height: 5),
+                Text(tabs[index].text),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
